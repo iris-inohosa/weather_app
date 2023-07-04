4cda5a74db49c57f790920b3034e74f4e7df4933
iris-inohosa <irisinohosa@gmail.com>
2023-07-04 09:01:36 +0000

    get current data from api

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -22,15 +22,29 @@
             let cityId = $(this).attr('city-id')
             fetch(`/get-current-weather/${cityId}`).then(resp => {
                 resp.json().then(data => {
+                    $(this).val('');
                     // get weather data and update html values
-                    console.log(data);
-                    $('#today-title').html('Today in ' + city);
-                    $('#temperature').html(data.temperature);
-
+                    updateUI(data, city);
+                    // reset autocompletet term value
+                    $('#city-search').data().uiAutocomplete.term = null;
                 })
             })
         });
     });
+
+    function updateUI(data, city) {
+        $('.weather-current').removeClass('hide-block');
+        $('.weather-forecast').removeClass('hide-block');
+        $('#today-title').html(`<b>${city}</b>`);
+        $('#temperature').html(data.temperature + '°C');
+        $('.extras').html('');
+        $('.extras').append(`<li class='p-1'><i class="fa-solid fa-droplet px-2"></i>Humidity: ${data.relHumidity}%</li>`);
+        $('.extras').append(`<li class='p-1'><i class="fa-solid fa-wind px-2"></i>Wind: ${data.windDirString} ${data.windSpeed}km/h</li>`);
+        $('.extras').append(`<li class='p-1'><i class="fa-solid fa-cloud px-2"></i>Cloudiness: ${data.cloudiness}%</li>`);
+        $('.extras').append(`<li class='p-1'><i class="fa-solid fa-temperature-quarter px-2"></i>Pressure: ${data.pressure}Pa</li>`);
+        $('#weather-symbol').attr('src', `https://developer.foreca.com/static/images/symbols/${data.symbol}.png`);
+        $('.symbol-phrase').html(`${data.symbolPhrase}, feels like ${data.feelsLikeTemp}°C`);
+    }
 
 
 })();
--- app/views.py
+++ app/views.py
@@ -20,7 +20,8 @@ def get_current_weather(city_id):
     }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        data = response.json()
+        data = response.json()['current']
+        print(data)
         return data
     else:
         return {'Failed': response.status_code}
@@ -41,8 +42,31 @@ def get_cities():
         locations = response.json()['locations'][:15]
         result = []
         for city in locations:
+            if 'adminArea' in city.keys():
+                city_label = f"{city['name']}, {city['country']}, {city['adminArea']}"
+            else:
+                city_label = f"{city['name']}, {city['country']}"
+            city_value = city['id']
             result.append(
-                {'label': f"{city['name']}, {city['country']}", 'value': city['id']})
+                {'label': city_label, 'value': city_value})
         return result
+    else:
+        return {'Failed': response.status_code}
+
+
+@views.route('api/get-next-days-forecast/<city_id>')
+def get_forecast(city_id):
+    url = f"https://foreca-weather.p.rapidapi.com/forecast/daily/{city_id}"
+    querystring = {"periods": "12"}
+    headers = {
+        "X-RapidAPI-Key": "fea3ded0c9msh3e73fe94cc634c7p164599jsn4956791f6a6a",
+        "X-RapidAPI-Host": "foreca-weather.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        return data
     else:
         return {'Failed': response.status_code}
